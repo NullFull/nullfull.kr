a0ac3b056b89129268fbdf973c2e28b9cfd08abf
lexifdev <sl@lxf.kr>
2019-12-02 21:49:38 +0900

    장족의 발전(?)

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,49 +5,57 @@ import './style.css'
 const Index = () => {
     return (
         <div>
-            <h1>널채움</h1>
+            <header>
+                <h1>널채움/* TODO : 여기에 로고 이미지 들어감 */</h1>
+                <h3>이 웹사이트는 리뉴얼 중이랍니다.</h3>
+                <p><strike>하는 일 없이</strike> 모여서 감자튀김 먹는 모임</p>
+            </header>
             <hr />
-            <div>
-
-            </div>
-            <hr />
-            <div>
-                <h3>웹사이트의 목적 (+리뉴얼의 목적)</h3>
-                <ul>
-                    <li>웹사이트를 보고 모임에 참석하고 싶게 한다</li>
-                    <li>우리가 그동안 만든거 자랑한다</li>
-                    <li>모임에 안와본 사람이 보고 당황하지 않아야 함</li>
-                </ul>
-            </div>
-            <hr />
-            <div>
-                <h3>웹사이트 구성요소</h3>
-                <ul>
-                    <li>슬랙 초대 링크</li>
-                    <li>페이스북 링크</li>
-                    <li>Github 링크</li>
-                    <li>프로젝트 목록</li>
-                    <li>하고 싶은 주제</li>
-                </ul>
-            </div>
-            <hr />
-            <h3>TODO</h3>
-            <ul>
-                <li><strike>배포</strike> -> 완료</li>
-                <li>
-                    디자인
+            <main>
+                <section className="lead-image">
+                    /* 여기에 Lead 이미지 (감자튀김) 들어감 */
+                </section>
+                <hr />
+                <section>
+                    <h3>다음 모임 공지</h3>
+                    <p>/* TODO : 여기에 다음 이벤트 공지를 페이스북에서 긁어와서 표시 */</p>
+                </section>
+                <hr />
+                <section>
+                    <h3>웹사이트의 목적 (리뉴얼의 목적)</h3>
                     <ul>
-                        <li>로고</li>
-                        <li>리드 이미지</li>
-                        <li><strike>푸터</strike></li>
-                        <li>또 뭐 해야되지...</li>
+                        <li>웹사이트를 보고 모임에 참석하고 싶게 한다. -> 다음 모임 공지 표시</li>
+                        <li>우리가 그동안 만든거 자랑한다</li>
+                        <li>모임에 안와본 사람이 보고 당황하지 않아야 함</li>
                     </ul>
-                </li>
-                <li>GA달기</li>
-            </ul>
-
+                    <h3>TODO</h3>
+                    <ul>
+                        <li><strike>배포</strike> -> 완료</li>
+                        <li>
+                            디자인
+                            <ul>
+                                <li>로고</li>
+                                <li>리드 이미지</li>
+                                <li><strike>푸터</strike></li>
+                                <li>또 뭐 해야되지...</li>
+                            </ul>
+                        </li>
+                        <li>GA달기</li>
+                        <li>
+                            들어갈 내용
+                            <ul>
+                                <li>슬랙 초대 링크</li>
+                                <li>페이스북 링크</li>
+                                <li>Github 링크</li>
+                                <li>프로젝트 목록</li>
+                                <li>하고 싶은 주제</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </section>
+            </main>
             <footer>
-                <p>All Right Reserved? 2019. Nullfull. </p>
+                <p>All Right Reserved? 2019. NullFull. </p>
             </footer>
         </div>
     )
